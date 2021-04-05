473d04fcd3bbe44dbc4598d78824929ee902c191
olaftarlowski <olaf.tarlowski@gmail.com>
2021-04-05 11:16:54 +0200

    basic burger layout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,13 @@
 import { Component } from "react";
+import Burger from '../../components/Burger/Burger'
+
+
 
 class BurgerBuilder extends Component {
     render () {
         return (
             <>
-                <div>Burger</div>
+                <Burger/>
                 <div>Build control</div>
             </>
         );
